c6f5fb8d1b0bfaa73e571bb3fc98b1b09e58bb5d
Mattias1610 <80158195+Mattias1610@users.noreply.github.com>
2024-08-11 13:19:55 +0300

    Update Program.cs

--- DobaiRobertMattias.MathGame/DobaiRobertMattias.MathGame/Program.cs
+++ DobaiRobertMattias.MathGame/DobaiRobertMattias.MathGame/Program.cs
@@ -4,13 +4,11 @@
 Console.WriteLine("Hello!");
 Console.WriteLine("What's your name?");
 var name = Console.ReadLine();
-Console.WriteLine($"How old are you, {name}?");
-var age = Console.ReadLine();
 var date = DateTime.Now;
 Console.WriteLine($"Today is {date}\n");
 
 Console.WriteLine("------------------------WELCOME TO THE MATH GAME------------------------");
-Console.WriteLine("Are you ready????????? Y/N");
+Console.WriteLine($"Are you ready, {name}????????? Y/N");
 var choice = Console.ReadLine();
 choice = choice.ToUpper();
 
@@ -18,5 +16,5 @@ if (choice == "Y")
 {
     Console.WriteLine("Okay, good");
     Game obj = new Game();
-    obj.gameStart();
+    obj.GameStart();
 }
